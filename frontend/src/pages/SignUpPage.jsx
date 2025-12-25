b518c80d55fb85398e16cea3235f94f06473b79c
JASH MANDALIYA <167299436+Jashmandaliya18@users.noreply.github.com>
2025-12-25 23:34:12 +0530

    feat: add email and password fields to SignUpPage with icons and toggle visibility for password

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { useAuthStore } from '../store/useAuthStore.js';
-import { MessageSquare, User } from "lucide-react"
+import { MessageSquare, User, Mail, Lock, EyeOff, Eye } from "lucide-react"
 
 const SignUpPage = () => {
   const [showPassword, setShowPassword] = useState(false);
@@ -51,6 +51,55 @@ const SignUpPage = () => {
                 />
               </div>
             </div>
+
+            <div className='form-control'>
+              <label className='label'>
+                <span className='label-text font-medium'>Email</span>
+              </label>
+              <div className='relative'>
+                <div className='absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none z-10'>
+                  <Mail className='size-5 text-base-content/40' />
+                </div>
+                <input
+                  type="text"
+                  className={`input input-bordered w-full pl-10`}
+                  placeholder='you@example.com'
+                  value={formData.email}
+                  onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+                />
+              </div>
+            </div>
+
+
+            <div className='form-control'>
+              <label className='label'>
+                <span className='label-text font-medium'>Password</span>
+              </label>
+              <div className='relative'>
+                <div className='absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none z-10'>
+                  <Lock className='size-5 text-base-content/40' />
+                </div>
+                <input
+                  type={showPassword ? "text" : "password"}
+                  className={`input input-bordered w-full pl-10`}
+                  placeholder='••••••••'
+                  value={formData.password}
+                  onChange={(e) => setFormData({ ...formData, password: e.target.value })}
+                />
+                <button
+                  type='button'
+                  className='absolute inset-y-0 right-0 pr-3 flex items-center'
+                  onClick={() => setShowPassword(!showPassword)}
+                >
+                  {showPassword ? (
+                    <EyeOff className='size-5 text-base-content/40' />
+                  ) : (
+                    <Eye className="size-5 text-base-content/40" />
+                  )}
+                </button>
+              </div>
+            </div>
+
           </form>
 
         </div>
