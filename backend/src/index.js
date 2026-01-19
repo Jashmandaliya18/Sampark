2d3fdd3345ebe71364b2930d4343823ca9f96c0e
JASH MANDALIYA <167299436+Jashmandaliya18@users.noreply.github.com>
2026-01-19 17:11:58 +0530

    Update CORS configuration to allow multiple origins

--- backend/src/index.js
+++ backend/src/index.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cookieParser());
 app.use(cors(
     {
-        origin: "http://localhost:5173" || "*",
+        origin: ["http://localhost:5173", "https://sampark-2.onrender.com" , "*"],
         credentials: true,
     }
 ))
